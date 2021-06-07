dee77041f2cb3376d48a908e48368e2999515a0e
soulikk <enclumek23@naver.com>
2021-06-07 22:44:15 +0900

    Set model, loginChk router

---
+++ models/index.js
@@ -0,0 +1,15 @@
+'user strict';
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config
+);
+
+const db = {};
+db.sequelize = sequelize;
+
+
+module.export = db;
