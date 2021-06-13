6767f5e7defdd0a71433af67f95762a8bb8f80fd
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-13 23:35:39 +0900

    set article router

---
+++ routes/article.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const { Article } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads');
+  fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+  }),
+  filename(req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  },
+  limits: { filename: 5 * 1024 * 1024 }
+})
