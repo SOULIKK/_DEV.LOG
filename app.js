d87a480cbcda82bc84636426dbda438466238ac7
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-16 21:07:13 +0900

    set passport

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const pageRouter = require('./routes/page');
+const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+
+dotenv.config();
+
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
 
@@ -10,7 +23,13 @@ app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true
-})
+});
+
+passportConfig();
+
+app.use(morgan('dev'));
+
+
 
 app.use('/', pageRouter);
 
