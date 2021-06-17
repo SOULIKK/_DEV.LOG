53dfef04dcf2db0aa4adcda2e6c68cbe22087292
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-17 20:37:42 +0900

    set static file

--- app.js
+++ app.js
@@ -1,37 +1,76 @@
 const express = require('express');
-const pageRouter = require('./routes/page');
+const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const session = require('express-session');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 const passport = require('passport');
 
 dotenv.config();
-
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const articleRouter = require('./routes/article');
 
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
 const app = express();
-
 app.set('port', process.env.PORT || 8008);
 app.set('view engine', 'html');
-
 nunjucks.configure('views', {
   express: app,
-  watch: true
+  watch: true,
 });
 
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('DB 연결 성공');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 passportConfig();
 
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
+app.use('/article', articleRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 존재하지 않음`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), 'Port Listening');
