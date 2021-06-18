4eec429f1fdcd789ad51fbbb291d408fa0a131c1
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-18 20:37:28 +0900

    set user model, create user router

--- models/index.js
+++ models/index.js
@@ -4,12 +4,24 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 
+const User = require('./user');
+const Article = require('./article');
+
+
 const sequelize = new Sequelize(
-  config.database, config.username, config.password, config
+  config.database, config.username, config.password, config,
 );
 
 const db = {};
 db.sequelize = sequelize;
 
+db.User = User;
+db.Article = Article;
 
-module.export = db;
+User.init(sequelize);
+Article.init(sequelize);
+
+User.associate(db);
+Article.associate(db);
+
+module.exports = db;
---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+  const { userId, nickName, password } = req.body;
+  // console.log(`아이디 :::::::::::::::: ${req.body.userId}`);
+  // console.log(`닉네임 :::::::::::::::: ${req.body.nickName}`);
+  try {
+    const exUser = await User.findOne({ where: { userId } });
+    if (exUser) {
+      return res.redirect('/join?error=exist');
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      userId,
+      nickName,
+      password: hash,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    console.log(`유저 :::::::::::::::: ${user}`);
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/');
+      // return res.send(user);
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
+module.exports = router;
