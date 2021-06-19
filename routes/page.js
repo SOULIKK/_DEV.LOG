96a40be468bc84769a562fddf553901096578ef7
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-19 23:33:10 +0900

    signup page

--- routes/page.js
+++ routes/page.js
@@ -7,4 +7,8 @@ router.get('/', (req, res, next) => {
   res.render('main', {});
 });
 
+router.get('/join', isNotLoggedIn, async (req, res, next) => {
+  res.render('join');
+})
+
 module.exports = router;
