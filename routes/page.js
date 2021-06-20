b8c16d81661873d533bded888ea6f11c1fc6f6b2
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-20 23:54:14 +0900

    update page router

--- routes/page.js
+++ routes/page.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+const { Article, User } = require('../models');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+})
 
-router.get('/', (req, res, next) => {
-  res.render('main', {});
+router.get('/', async (req, res, next) => {
+  try {
+    const articles = await Article.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nickname']
+      },
+      order: [
+        ['createdAt', 'DESC']
+      ],
+    });
+    res.render('main', {
+      titile: '_DEV.LOG',
+      articles: articles
+    })
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
-router.get('/join', isNotLoggedIn, async (req, res, next) => {
+router.get('/join', isLoggedIn, async (req, res, next) => {
   res.render('join');
 })
 
-module.exports = router;
