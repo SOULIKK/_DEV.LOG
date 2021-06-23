c834eedc4b40235846fcf9804f07e20989a3b140
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-06-23 23:49:45 +0900

    set user router

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
 
 const router = express.Router();
 
-
+router.post('/:id', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      res.send('success');
+    } else {
+      res.status(404).send('존재하지 않는 유저');
+    }
+  } catch (err) {
+    console.error(err)
+    next(err);
+  }
+});
 
 module.exports = router;
